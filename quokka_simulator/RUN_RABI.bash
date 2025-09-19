85a572cb795dc87ca2a5b70fbbe2f2c1e239a63b
wamud <nawaran1@outlook.com>
2025-09-19 17:03:00 +1000

    modified runs in run_rabi.bash

--- quokka_simulator/RUN_RABI.bash
+++ quokka_simulator/RUN_RABI.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # p (physical error rate) values:
-p_values=(0.1)
+p_values=(0 0.0005 0.001 0.002 0.003 0.004)
 
 
 for p in "${p_values[@]}"; do
@@ -9,8 +9,8 @@ for p in "${p_values[@]}"; do
 
 	echo "Running p = $p rabi simulations..."
 
-	for i in $(seq 0 0); do
-   	 ./rabi_simulator_modules/d3rabi_v4.out 3 0.25 0 $p 2 10 > /dev/null &
+	for i in $(seq 0 4); do
+   	 ./rabi_simulator_modules/d3rabi_v4.out 3 0.25 0 $p 2 1000 > /dev/null &
 	done
 
 	wait
