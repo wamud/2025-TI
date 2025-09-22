ab0b2d9af18d70087eb8e287e47c9109935479ab
Anthony O'Rourke <anthony.orourke@student.uts.edu.au>
2025-09-22 20:45:28 +1000

    increased reps

--- quokka_simulator/RUN_RABI.bash
+++ quokka_simulator/RUN_RABI.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # p (physical error rate) values:
-p_values=(0 0.0005 0.001 0.002 0.003 0.004)
+p_values=(0)
 
 
 for p in "${p_values[@]}"; do
@@ -10,7 +10,7 @@ for p in "${p_values[@]}"; do
 	echo "Running p = $p rabi simulations..."
 
 	for i in $(seq 0 10); do
-   	 ./rabi_simulator_modules/d3rabi_v4.out 3 0.25 0 $p 2 100000 &
+   	 ./rabi_simulator_modules/d3rabi_v4.out 3 0.25 0 $p 2 1000000 &
 	done
 
 	wait
